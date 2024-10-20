9044eeb9e9534dc6e51a8c44b94712a71a836ac3
Kenc1k <iskandarisabrkov@gmail.com>
2024-10-20 19:58:53 +0500

    "laravel first"

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/' , function (){
+Route::get('/index', function () {
     return view('index');
 });
-Route::get('/about' , function (){
-    return view('about');
+
+Route::get('/index2', function () {
+    return view('index2');
 });
-Route::get('/contact' , function (){
-    return view('contact');
+
+
+Route::get('/index3', function () {
+    return view('index3');
 });
+
+
+Route::get('/main', function () {
+    return view('main');
+});
+
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+
+
