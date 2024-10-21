920b88512b61076ddbd6c5547979c748d2bb0e77
Kenc1k <iskandarisabrkov@gmail.com>
2024-10-21 15:07:55 +0500

    "shuncaki darsga aloqasi yoq"

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -172,25 +172,6 @@
             </ul>
           </li>
 
-          <li class="nav-header">EXAMPLES</li>
-          <li class="nav-item">
-            <a href="/calendar" class="nav-link">
-              <i class="nav-icon far fa-calendar-alt"></i>
-              <p>
-                Calendar
-                <span class="badge badge-info right">2</span>
-              </p>
-            </a>
-          </li>
-
-          <li class="nav-item">
-            <a href="/gallery" class="nav-link">
-              <i class="nav-icon far fa-calendar-alt"></i>
-              <p>
-                Gallery
-              </p>
-            </a>
-          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
